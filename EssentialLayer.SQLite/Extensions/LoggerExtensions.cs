5e1104bde049054f85fe88afac1595a47dc2435c
Mario <mariosotomor.dev@gmail.com>
2026-04-15 18:05:27 -0600

    refactor: New extensions asbtraction of ILogger

---
+++ EssentialLayer.SQLite/Extensions/LoggerExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace EssentialLayer.SQLite.Extensions
+{
+	public static class LoggerExtensions
+	{
+		public static void LogInfo(
+			this ILogger logger,
+			string message,
+			[CallerMemberName] string memberName = ""
+		)
+		{
+			logger.LogInformation(
+				"Method: {MemberName} | Message: {Message}",
+				memberName,
+				message
+			);
+		}
+
+		public static void LogErrorEx(
+			this ILogger logger,
+			Exception e,
+			[CallerMemberName] string memberName = ""
+		)
+		{
+			logger.LogError(
+				e,
+				"Method: {MemberName} | Message: {Message}",
+				memberName,
+				e.Message
+			);
+		}
+	}
+}
